f41a657625f32768e803c5bd93b05ea259c20cf6
HugoLifes12 <HVidana.A@tomza.com>
2023-08-30 14:03:14 -0600

    Api train

---
+++ DtFire/TodoDb.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        :base(options){}
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
