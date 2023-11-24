bee404d2db91a2d03b7c54ce312dbec05d507e7f
00lar <eduardani84@gmail.com>
2023-11-23 18:28:06 -0700

    fix dependences

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
-import starflut
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  StarflutPlugin.register(with: registry.registrar(forPlugin: "StarflutPlugin"))
 }
