51eef69bca26610a8fba6cd3099177d7fc65a939
00lar <eduardani84@gmail.com>
2023-12-01 11:18:25 -0700

    env variables

--- server/lib/postgres.pool.js
+++ server/lib/postgres.pool.js
@@ -1,12 +1,14 @@
 const { Pool } = require('pg');
 
+const {config} = require("../config/config")
+
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+
 
   const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'dani',
-    password: 'daniadmin123',
-    database: 'ultron',
+    connectionString: URI
   });
 
 
