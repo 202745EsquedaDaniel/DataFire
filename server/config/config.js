9d4cb24178f6b8afd546255a5844c64315b4127a
00lar <eduardani84@gmail.com>
2023-12-05 13:50:48 -0700

    fix v2

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
-  dbUrl: process.env.DATABASE_URL
+  dbUrl: process.env.DATABASE_URL,
 };
 
 module.exports = { config };
