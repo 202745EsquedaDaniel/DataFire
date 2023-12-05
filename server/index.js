dc2535e17d35dc6da0310fbe575285d01e1093ba
00lar <eduardani84@gmail.com>
2023-12-05 13:55:05 -0700

    cors and fix port

--- server/index.js
+++ server/index.js
@@ -1,19 +1,34 @@
 const express = require('express');
-
+const cors = require("cors")
 const routerApi = require('./routes');
+
 const {logErrors, errorHandler, boomHandler, ormErrorHandler} = require("./middlewares/error.handler")
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
+
+
+app.use(express.json());
 
 console.clear();
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello server');
 });
 
 
-app.use(express.json());
 
 routerApi(app);
 
