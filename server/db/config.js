2ea2aa1c5da7dbb8190f007b5fd2a095216ab0b3
00lar <eduardani84@gmail.com>
2023-12-06 10:48:52 -0700

    migration config

--- server/db/config.js
+++ server/db/config.js
@@ -1,17 +1,17 @@
+// db/config.js
 const { config } = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-  const PASSWORD = encodeURIComponent(config.dbPassword);
-  const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-  options.connectionString = URI;
-
-  module.exports = {
-    development: {
-      url: URI,
-      dialect: 'postgres',
-    },
-    production: {
-      url: URI,
-      dialect: 'postgres',
+module.exports = {
+  development: {
+    url: config.dbUrl,
+    dialect: 'postgres',
+    // Other development options...
+  },
+  production: {
+    url: config.dbUrl,
+    dialect: 'postgres',
+    ssl: {
+      rejectUnauthorized: false
     }
   }
+};
