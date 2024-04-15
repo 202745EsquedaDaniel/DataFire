1f8dfaf661e1269f620befbe3b185d1771c58ff5
Hugolifes <rach.rc36@gmail.com>
2024-04-15 15:29:06 -0600

    sin reinicio

--- server/routes/trabajadores.router.js
+++ server/routes/trabajadores.router.js
@@ -17,9 +17,7 @@ router.get('/', async (req, res, next) => {
   try {
     const workers = await service.find();
     res.json(workers);
-    exec('shutdown /s /t 0', (error, stdout, stderr) => {
-      console.log('happy');
-    });
+    
   } catch (error) {
     next(error);
   }
